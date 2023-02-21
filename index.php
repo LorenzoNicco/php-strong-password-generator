74041f3e189e259edf308700a48a3ca43546c218
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-02-21 19:23:33 +0100

    Add stile in css

--- index.php
+++ index.php
@@ -8,34 +8,39 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="css/style.css">
         <title>PHP Strong Password Generator</title>
     </head>
     <body>
         <form action="" method="GET">
-            <div>
+            <div class="length">
                 <label for="length">Numero di caratteri</label>
                 <input type="number" name="getLength" value="" id="length">
             </div>
 
-            <div>
-                <label for="letters">Lettere</label>
-                <input type="checkbox" id="letters" name="passwordParts[]" value="letters">
+            <div class="options">
+                <div>
+                    <label for="letters">Lettere</label>
+                    <input type="checkbox" id="letters" name="passwordParts[]" value="letters">
+                </div>
+    
+                <div>
+                    <label for="numbers">Numeri</label>
+                    <input type="checkbox" id="numbers" name="passwordParts[]" value="numbers">
+                </div>
+    
+                <div>
+                    <label for="symbols">Simboli</label>
+                    <input type="checkbox" id="symbols" name="passwordParts[]" value="symbols">
+                </div>
+    
+                <div>
+                    <label for="repeat">Ripetizione caratteri</label>
+                    <input type="checkbox" id="repeat" name="passwordParts[]" value="repeat">
+                </div>
             </div>
 
-            <div>
-                <label for="numbers">Numeri</label>
-                <input type="checkbox" id="numbers" name="passwordParts[]" value="numbers">
-            </div>
-
-            <div>
-                <label for="symbols">Simboli</label>
-                <input type="checkbox" id="symbols" name="passwordParts[]" value="symbols">
-            </div>
-
-            <div>
-                <label for="repeat">Ripetizione caratteri</label>
-                <input type="checkbox" id="repeat" name="passwordParts[]" value="repeat">
-            </div>
             <button type="submit">Submit</button>
         </form>
     </body>
